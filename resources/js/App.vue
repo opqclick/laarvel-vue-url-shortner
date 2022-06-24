f47517cf9348d2a54a43d774eadb2df8470033ed
Opu Hasnat <opqclick@gmail.com>
2022-06-25 01:29:32 +0600

    Another url-shortner projected initiated

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <section class="h-screen">
+        <Form/>
+    </section>
+</template>
+<script>
+import Form from "./components/Form.vue";
+
+export default {
+    components: {
+        Form,
+    },
+};
+</script>
